c28df1f781d2b0743161de67ba9ba61215a2d5cd
Jaya435 <thomasjaya@icloud.com>
2020-10-01 20:21:19 +0100

    minor changes to aid deployment

--- api/manage.py
+++ api/manage.py
@@ -3,7 +3,6 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from src.app import create_app, db
-from src.models import BlogpostModel, UserModel
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
